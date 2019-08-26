645e62c91bb26937570f31f9e1f8160e0bf686fd
Michael Chen <>
2019-08-25 23:55:10 -0700

    Remove unused reference

--- src/components/AboutButton.tsx
+++ src/components/AboutButton.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Dialog from "@material-ui/core/Dialog";
-import Typography from "@material-ui/core/Typography";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import { DialogContentText, IconButton, Link } from "@material-ui/core";
