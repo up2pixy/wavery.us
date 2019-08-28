b7d2e8ebe665cf81ff817744b027dc99e34428ee
Michael Chen <>
2019-08-27 22:11:43 -0700

    Update the BMC link

--- src/components/BuyCoffeeButton.tsx
+++ src/components/BuyCoffeeButton.tsx
@@ -4,7 +4,7 @@ import withWidth, { isWidthUp, WithWidth, isWidthDown } from "@material-ui/core/
 const BuyCoffeeButtonC = (props: WithWidth) => {
   return (
     <div style={{ margin: "0px 20px" }}>
-      <a className="bmc-button" target="_blank" href="https://www.buymeacoffee.com/StbkayK">
+      <a className="bmc-button" target="_blank" href="https://www.buymeacoffee.com/MichaelC">
         <img
           src="https://bmc-cdn.nyc3.digitaloceanspaces.com/BMC-button-images/BMC-btn-logo.svg"
           alt="Buy me a coffee"
